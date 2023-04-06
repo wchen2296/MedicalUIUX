a30187b7d5b26105415d0abff05c33e7cc7e6e1b
wchen2296 <53542192+wchen2296@users.noreply.github.com>
2023-04-05 20:14:22 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import './App.css';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function About() {
+  return <h1>About Us</h1>;
+}
+
+function Appointments() {
+  return <h1>Appointment Requests</h1>;
+}
+
+function Reviews() {
+  return <h1>Reviews</h1>;
+}
 
 function App() {
   return (
-    <div>
-     <Navbar/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/about" component={About} />
+        <Route path="/appointments" component={Appointments} />
+        <Route path="/reviews" component={Reviews} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,36 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CloseIcon from '@mui/icons-material/Close';
 import { Box } from '@mui/system';
+import {Drawer, List, ListItem} from '@mui/material'
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    setDrawerOpen(open);
+  };
+
+  const drawerLinks = (
+    <Box className="drawer-content">
+      <IconButton
+        className="close-button"
+        size="large"
+        color='inherit'
+        edge="end"
+        aria-label="close"
+        onClick={toggleDrawer(false)}
+      >
+        <CloseIcon />
+      </IconButton>
+      <List className="drawer-list">
+        <ListItem button>
+          <NavLink to="/about" className="navlink" activeClassName="activeNavlink" onClick={toggleDrawer(false)}>
+            About Us
+          </NavLink>
+        </ListItem>
+        <ListItem button>
+          <NavLink to="/appointments" className="navlink" activeClassName="activeNavlink" onClick={toggleDrawer(false)}>
+            Appointment Requests
+          </NavLink>
+        </ListItem>
+        <ListItem button>
+          <NavLink to="/reviews" className="navlink" activeClassName="activeNavlink" onClick={toggleDrawer(false)}>
+            Reviews
+          </NavLink>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar className="navbar">
+          <Box display="flex" alignItems="center">
+           
+            <NavLink to="/" className="navlink" activeClassName="activeNavlink">
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+               Vision World
+              </Typography>
+            </NavLink>
+
+          </Box>
+          <Box className={`navbar-links ${drawerOpen ? 'hidden' : ''}`}>
+            <NavLink to="/about" className="navlink" activeClassName="activeNavlink" sx={{ marginRight: 2 }}>
+              About Us
+            </NavLink>
+            <NavLink to="/appointments" className="navlink" activeClassName="activeNavlink" sx={{ marginRight: 2 }}>
+              Appointment Requests
+            </NavLink>
+            <NavLink to="/reviews" className="navlink" activeClassName="activeNavlink">
+              Reviews
+            </NavLink>
+          </Box>
           <IconButton
+            className="menu-button"
             size="large"
-            edge="start"
+            edge="end"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
-          <LocalHospitalIcon fontSize="large" sx={{ mr: 2 }} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Healthcare Site
-          </Typography>
-          <IconButton size="large" color="inherit">
-            <AccountCircleIcon />
-          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)} classes={{ paper: 'drawer' }}>
+        {drawerLinks}
+      </Drawer>
     </Box>
   );
 };
